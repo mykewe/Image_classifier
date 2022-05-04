6862eae3319381af31d78d90f4f3567e0a4041fe
Michael Ewetola <61256058+mykewe@users.noreply.github.com>
2022-05-04 18:21:57 +0100

    app created

--- web_app.py
+++ web_app.py
@@ -4,12 +4,13 @@ from PIL import Image
 
 st.title("Flower spicie Classification")
 st.header("Flower spicie Classification")
+st.caption("This image classifier recognizes different species of flowers. It tells you the most likely spicie your flower belongs to and the corresponding probability.")
 st.text("Upload a flower image whose spicie you wish to predict")
 
 
 uploaded_file = st.file_uploader("Choose a flower image ...", type="jpg")
-top_k = st.number_input('Insert a number for the top most likely classes',min_value=1, max_value=5,value=1, step=1)
-st.write('You want the top', top_k, 'most likely classes')
+top_k = st.number_input('Insert a number for the top most likely class(es)',min_value=1, max_value=5,value=1, step=1)
+st.write('You want the top', top_k, 'most likely class(es)')
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Flower', use_column_width=True)
